84dce82eadb15f44f71655819c5f0d38e2a91857
shakurlotfy <46642773+shakurlotfy@users.noreply.github.com>
2023-02-23 23:17:22 +0330

    Add files via upload

---
+++ UseJsonParserMultipla/UseJsonParserMultipla/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UseJsonParserMultipla
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btn_convert_1_Click(object sender, EventArgs e)
+        {
+            string JsonStr = "{\"id\":\"1\",\"name\":\"Jack\",\"age\":\"36\"}";
+
+            JsonParser.JParser JP = new JsonParser.JParser();
+            DataTable dt = JP.JsonConvert(JsonStr);
+        }
+    }
+}
